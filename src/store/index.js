b3c5c2507b946f1d15ba35aa1ca5c1c8b10ae095
chaosmos <vitovich1987@gmail.com>
2023-03-21 19:28:17 +0300

    finished the project

--- src/store/index.js
+++ src/store/index.js
@@ -2,14 +2,18 @@ import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
 import { usersReducer } from './slices/usersSlice';
 import { albumsApi } from './api/albumsApi';
+import { photosApi } from './api/photosApi';
 
 export const store = configureStore({
   reducer: {
     users: usersReducer,
-    [albumsApi.reducerPath]: albumsApi.reducer
+    [albumsApi.reducerPath]: albumsApi.reducer,
+    [photosApi.reducerPath]: photosApi.reducer
   },
   middleware: (getDefaultMiddleware) => {
-    return getDefaultMiddleware().concat(albumsApi.middleware);
+    return getDefaultMiddleware()
+        .concat(albumsApi.middleware)
+        .concat(photosApi.middleware);
   }
 });
 
@@ -18,4 +22,15 @@ setupListeners(store.dispatch);
 export * from './thunks/fetchUsers';
 export * from './thunks/addUser';
 export * from './thunks/removeUser';
-export { useFetchAlbumsQuery, useAddAlbumMutation, useRemoveAlbumMutation} from './api/albumsApi';
+
+export {
+  useFetchAlbumsQuery,
+  useAddAlbumMutation,
+  useRemoveAlbumMutation
+} from './api/albumsApi';
+
+export {
+  useFetchPhotosQuery,
+  useAddPhotoMutation,
+  useRemovePhotoMutation
+} from './api/photosApi';
